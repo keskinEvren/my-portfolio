3f16e2ed0892a5b4348911d39d09ce69524ad641
Evren Keskin <evrenkeskin0998@gmail.com>
2024-06-20 14:17:55 +0300

    Updates

---
+++ precache-manifest.1e41ef2b1c08c2c4ef5c2be3cd2dc8b1.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44085f71b070ee192bd85f4b9f7ce620",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "7ee5a8c2eb64b2c90e98",
+    "url": "/my-portfolio/static/css/main.88b7011e.chunk.css"
+  },
+  {
+    "revision": "275333903e9a6efdfe1e",
+    "url": "/my-portfolio/static/js/2.19ff3d85.chunk.js"
+  },
+  {
+    "revision": "84f48c9a090ba2f8cc4a11e48dd3dad0",
+    "url": "/my-portfolio/static/js/2.19ff3d85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ee5a8c2eb64b2c90e98",
+    "url": "/my-portfolio/static/js/main.47612171.chunk.js"
+  },
+  {
+    "revision": "5d4511f3fcec99c33304",
+    "url": "/my-portfolio/static/js/runtime-main.c58b4907.js"
+  },
+  {
+    "revision": "c60f0dfee2f6270432487dc2f3a23499",
+    "url": "/my-portfolio/static/media/coffee-apple.c60f0dfe.jpg"
+  },
+  {
+    "revision": "54fd423489a1937caf8950d12c8a8526",
+    "url": "/my-portfolio/static/media/html-css-javascript-lg.54fd4234.jpg"
+  },
+  {
+    "revision": "2c7701aae07adecdb0a7e336dff08def",
+    "url": "/my-portfolio/static/media/html-css-javascript.2c7701aa.jpg"
+  },
+  {
+    "revision": "730958a228d26dee011c5aa0aa3c3db9",
+    "url": "/my-portfolio/static/media/javascript-fullstack.730958a2.jpg"
+  },
+  {
+    "revision": "90616a5dc9acdcce7158addc89620052",
+    "url": "/my-portfolio/static/media/mern-stack.90616a5d.jpg"
+  },
+  {
+    "revision": "a8b012a0d7afd08ee07251e1f67294ff",
+    "url": "/my-portfolio/static/media/react-redux.a8b012a0.jpg"
+  },
+  {
+    "revision": "f370c1e0adb80194dd3ea10e468bc69a",
+    "url": "/my-portfolio/static/media/react.f370c1e0.png"
+  }
+]);
